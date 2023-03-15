119ac20e5dde4b466273f61cc782e09169d5cb57
tulvgengenr <871005218@qq.com>
2023-03-15 11:41:13 +0800

    update the install to add command in /etc/profile

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # install scow-sync globally by add soft link in /usr/local/bin
 # usage: sudo bash install.sh
-pip3 install paramiko==3.0.0
+# pip3 install paramiko==3.0.0
 local_path=$(pwd)
 chmod +x $local_path/scow-sync
-ln -s $local_path/scow-sync /usr/local/bin/scow-sync
-chmod +x /usr/local/bin/scow-sync
+echo "export PATH=\$PATH:$local_path" >> /etc/profile
+source /etc/profile
