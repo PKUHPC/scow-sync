4f025787df8e8005fa3c413564b707f9de218f9a
tulvgengenr <871005218@qq.com>
2023-03-16 15:47:00 +0800

    delete the pipe to scow-sync.py

--- scow-sync-start.py
+++ scow-sync-start.py
@@ -2,13 +2,13 @@
 Start a file transfer asynchronously
 '''
 import sys
-from subprocess import Popen, PIPE
+from subprocess import Popen
 from argsparser import ArgsParser
 
 
 if __name__ == '__main__':
     args = ArgsParser().get_args_parser().parse_args()
     cmd = f'python3 scow-sync.py -a {args.address} -u {args.user} -s {args.source} -d {args.destination} -m {args.max_depth} -p {args.port} -k {args.sshkey_path}'
-    popen = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+    popen = Popen(cmd, shell=True)
     sys.stdout.write(str(popen.pid))
-
+    exit(0)
