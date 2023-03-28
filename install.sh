5fe4ca8f958ad6ff395bee8b443b40478bcd7000
tulvgengenr <871005218@qq.com>
2023-03-28 10:46:54 +0800

    现在install.sh会自动安装最新版的rsync

--- install.sh
+++ install.sh
@@ -3,9 +3,25 @@
 # usage: sudo bash install.sh
 pip3 install psutil==5.9.4
 pip3 install paramiko==3.0.0
+if [[ -e /etc/os-release ]]; then
+    source /etc/os-release
+    if [[ $ID == "debian" || $ID == "ubuntu" ]]; then
+        apt-get install -y rsync
+    elif [[ $ID == "centos" || $ID == "rhel" ]]; then
+        yum install -y rsync
+    elif [[$ID == "alpine"]]; then
+        apk install -y rsync
+    else
+        echo "Unsupported Linux distribution"
+        exit 1
+    fi
+else
+    echo "Unsupported Linux distribution"
+    exit 1
+fi
 local_path=$(pwd)
 chmod +x $local_path/scow-sync-start
-chmod +X $local_path/scow-sync-query
+chmod +x $local_path/scow-sync-query
 ln -s $local_path/scow-sync-start /usr/bin/scow-sync-start
 ln -s $local_path/scow-sync-query /usr/bin/scow-sync-query
 ln -s $local_path/scow-sync /usr/bin/scow-sync
