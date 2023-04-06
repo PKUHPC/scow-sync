8fdf5266045f109212a2e7c22545572991105858
tulvgengenr <871005218@qq.com>
2023-04-06 10:22:22 +0800

    将python3和pip3的安装也放入了install.sh中

--- install.sh
+++ install.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 # install scow-sync globally by add soft link in /usr/local/bin
 # usage: sudo bash install.sh
-pip3 install psutil==5.9.4
-pip3 install paramiko==3.0.0
 if [[ -e /etc/os-release ]]; then
     source /etc/os-release
     if [[ $ID == "debian" || $ID == "ubuntu" ]]; then
-        apt-get install -y rsync
+        apt-get install -y rsync python3 pip3
     elif [[ $ID == "centos" || $ID == "rhel" ]]; then
-        yum install -y rsync
+        yum install -y rsync python3 pip3
     elif [[$ID == "alpine"]]; then
-        apk install -y rsync
+        apk install -y rsync python3 pip3
     else
         echo "Unsupported Linux distribution"
         exit 1
@@ -19,6 +17,8 @@ else
     echo "Unsupported Linux distribution"
     exit 1
 fi
+pip3 install psutil==5.9.4
+pip3 install paramiko==3.0.0
 local_path=$(pwd)
 chmod +x $local_path/scow-sync-start
 chmod +x $local_path/scow-sync-query
