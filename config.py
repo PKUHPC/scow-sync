b00d43192bea088aa7743965f5c61f6a8b9e7568
tulvgengenr <871005218@qq.com>
2023-04-19 16:50:35 +0800

    修改配置文件的默认路径

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ Config for scow-sync
 import os
 
 SCOWSYNC_PATH = os.path.expanduser('~/scow/.scow-sync')
-LOG_PATH = os.path.join(SCOWSYNC_PATH, 'scow-sync.out')
-ERROR_PATH = os.path.join(SCOWSYNC_PATH, 'scow-sync.out')
+LOG_PATH = os.path.join(SCOWSYNC_PATH, 'scow-sync.log')
+ERROR_PATH = os.path.join(SCOWSYNC_PATH, 'scow-sync.err')
